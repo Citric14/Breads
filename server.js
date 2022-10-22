05a57939761efa0b237377c6030d5cda3a4235d4
George <georgehender997@gmail.com>
2022-10-22 11:40:12 -0400

    finished with today

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
