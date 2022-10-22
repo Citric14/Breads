6447cf303f1900c6ff32fdaac6ab59dbdfe3e903
George <georgehender997@gmail.com>
2022-10-22 09:58:18 -0400

    part three done

--- views/index.jsx
+++ views/index.jsx
@@ -2,9 +2,9 @@ const React = require('react')
 const { lastIndexOf } = require('../models/bread')
 const Default = require('./layouts/default')
 
-function Index ({breads}) {
+function Index ({breads, title}) {
     return (
-        <Default>
+        <Default title={title}>
             <h2>Index Page</h2>
             {/* <p>I have {breads[0].name} bread! </p> */}
             <ul>
@@ -12,7 +12,9 @@ function Index ({breads}) {
                     breads.map((bread, index)=> {
                         return (
                         <li key={index}>
+                            <a href={`/breads/${index}`}>
                             {bread.name}
+                            </a>
                         </li>)
                     })
                 }
